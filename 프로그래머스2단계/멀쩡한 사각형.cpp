77b98fcad7a9b950ede53fd664d883af00d2bf0a
Piousangel <yyh7654@gmail.com>
2021-07-09 18:31:42 +0900

    Create 멀쩡한 사각형.cpp

---
+++ 프로그래머스2단계/멀쩡한 사각형.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+//수학문제 푼다고 생각하고 풀기..
+
+using namespace std;
+long long gcd(int w, int h){
+    long temp;
+        while(h != 0){
+            temp = w % h;
+            w = h;
+            h = temp;
+        } 
+    return w;
+}
+
+long long lcd(int w, int h){
+    return (w + h) - gcd(w,h);
+}
+
+long long solution(int w,int h) {
+    long long answer;
+    long long W = w;
+    long long H = h;
+    
+    answer = (W*H) - lcd(w,h);
+    
+    return answer;
+}
