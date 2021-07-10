bb2c054363352c351c6639b619e7fdc29e6987a7
Piousangel <yyh7654@gmail.com>
2021-07-11 01:16:49 +0900

    Create 나누어떨어지는숫자배열.cpp

---
+++ 프로그래머스1단계/나누어떨어지는숫자배열.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    int count = 0;
+    
+    for(int i=0; i< arr.size(); i++){
+        if(arr[i] % divisor == 0){
+            answer.push_back(arr[i]);
+        }
+        else
+            count++;
+    }
+    
+    if(count == arr.size()) answer.push_back(-1);
+    sort(answer.begin(), answer.end());
+    
+    
+    return answer;
+}
