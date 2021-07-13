2b4154c4d270ee825c88359b6629e93a4c6d4774
Piousangel <yyh7654@gmail.com>
2021-07-13 11:32:25 +0900

    Create 행렬의곱셈.cpp
    행곱, 소수찾기 복잡도 줄이기..

---
+++ 프로그래머스2단계/행렬의곱셈.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+//첫번째 행렬을 A, 두번째 행렬을 B, 결과 행렬을 C라고 해보자.
+//C의 i행 j열의 값 = A의 i행의 값 × B의 j열의 값          --> 기억하자 현석아..!
+
+vector<vector<int> > soultion(){
+    vector<vector<int> > answer;
+
+    return answer;
+}
+
+
