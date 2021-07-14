52bbd375882cefef1a6126cb93835b22e649277f
Piousangel <yyh7654@gmail.com>
2021-07-14 15:48:41 +0900

    Update 행렬의곱셈.cpp
    3for 위치 까먹지마세욧

--- 프로그래머스2단계/행렬의곱셈.cpp
+++ 프로그래머스2단계/행렬의곱셈.cpp
@@ -8,10 +8,23 @@ using namespace std;
 //첫번째 행렬을 A, 두번째 행렬을 B, 결과 행렬을 C라고 해보자.
 //C의 i행 j열의 값 = A의 i행의 값 × B의 j열의 값          --> 기억하자 현석아..!
 
-vector<vector<int> > soultion(){
+vector<vector<int> > solution(vector<vector<int> > arr1, vector<vector<int> > arr2) {
     vector<vector<int> > answer;
-
+    
+    for(int i=0; i < arr1.size(); i++){
+        vector<int> ans;
+        for(int j=0; j < arr2[0].size(); j++){
+            int sum = 0;
+            for(int k=0; k < arr1[0].size(); k++){
+                sum += arr1[i][k] * arr2[k][j];
+            }
+            ans.push_back(sum);
+        }
+        answer.push_back(ans);
+    }
+    
     return answer;
 }
+
 
 
