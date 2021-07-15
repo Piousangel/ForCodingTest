ba8eb5aa13af1d154175969d944df49f782bd246
Piousangel <yyh7654@gmail.com>
2021-07-15 19:35:07 +0900

    Create 땅따먹기.cpp
    re...p

---
+++ 프로그래머스2단계/땅따먹기.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+//다시 풀기..
+
+int max_return(vector<int>& vec, int a){
+    int temp = 0;
+    for(int i=0; i < 4; i++){
+        if(i != a){
+            temp = max(temp, vec[i]);
+        }
+    }
+    return temp;
+}
+
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+ 
+    for(int i=0; i < land.size(); i++){
+        for(int j=0; j < 4; j++){
+            land[i][j] += max_return(land[i-1],j);
+            answer = max(answer, land[i][j]);
+        }
+    }
+    return answer;
+}
