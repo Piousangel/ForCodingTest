93d997798ca3cee68282bd970d98f430b3d190c4
Piousangel <yyh7654@gmail.com>
2021-07-15 19:45:07 +0900

    Update 땅따먹기.cpp
    +그냥 3중포문 돌린거

--- 프로그래머스2단계/땅따먹기.cpp
+++ 프로그래머스2단계/땅따먹기.cpp
@@ -29,3 +29,27 @@ int solution(vector<vector<int> > land)
     }
     return answer;
 }
+
+//걍 삼중으로 돌린거
+// int solution(vector<vector<int> > land)
+//     {
+//         int answer = 0, m = 0;
+        
+//         for(int i = 1; i < land.size(); i++)
+//         {
+//             for(int j = 0; j < 4; j++)
+//             {
+//                 m = 0;
+//                 for(int k = 0; k < 4; k++)
+//                 {
+//                     if(j != k)
+//                         m = max(m, land[i - 1][k]);
+//                 }
+//                 land[i][j] += m;
+//             }
+//         }
+        
+//         for(int i = 0; i < 4; i++)
+//             answer = max(answer, land[land.size() - 1][i]);
+//         return answer;
+//     }
