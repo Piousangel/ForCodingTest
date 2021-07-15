1d03b18211e629f099f0bb456c17ef0d583ace3d
Piousangel <yyh7654@gmail.com>
2021-07-15 19:40:30 +0900

    Update 땅따먹기.cpp
    after -> dynamic pro..

--- 프로그래머스2단계/땅따먹기.cpp
+++ 프로그래머스2단계/땅따먹기.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 //다시 풀기..
+//동적계획법으로 풀어보기
 
 int max_return(vector<int>& vec, int a){
     int temp = 0;
@@ -21,7 +22,7 @@ int solution(vector<vector<int> > land)
 {
     int answer = 0;
  
-    for(int i=0; i < land.size(); i++){
+    for(int i=1; i < land.size(); i++){
         for(int j=0; j < 4; j++){
             land[i][j] += max_return(land[i-1],j);
             answer = max(answer, land[i][j]);
