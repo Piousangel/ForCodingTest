008a83bb0018a925cf4fb54158de19143c8db24b
Piousangel <yyh7654@gmail.com>
2021-07-27 00:06:14 +0900

    Create 음양더하기.java
    simple

---
+++ 프로그래머스1단계/음양더하기.java
@@ -0,0 +1,16 @@
+class 음양더하기 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0; i < absolutes.length; i++){
+            
+            if(signs[i] == false){
+                absolutes[i] = (-absolutes[i]);
+            }
+            
+            answer += absolutes[i];
+        }
+        
+        return answer;
+    }
+}
