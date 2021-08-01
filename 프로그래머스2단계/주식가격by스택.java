f90f8f2d204d95a1e408464fd5f4d9cf9f0017cd
Piousangel <yyh7654@gmail.com>
2021-08-01 18:52:13 +0900

    Create 주식가격by스택.java
    8월2일에 기능개발(Queue), 주식가격(Stack) 한번더 보기

---
+++ 프로그래머스2단계/주식가격by스택.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Stack;
+
+class 주식가격by스택 {
+    public int[] solution(int[] prices) {
+        Stack<Integer> st = new Stack();
+        
+        int[] answer = new int[prices.length];
+        
+        
+        for(int i=0; i< prices.length; i++){
+            while(!st.isEmpty() && prices[i] < prices[st.peek()]){
+                answer[st.peek()] = i - st.peek();
+                st.pop();
+            }
+            st.push(i);
+        }
+        
+        while(!st.isEmpty()){
+            answer[st.peek()] = prices.length - st.peek() - 1;
+            st.pop();
+        }
+        
+        return answer;
+    }
+}
