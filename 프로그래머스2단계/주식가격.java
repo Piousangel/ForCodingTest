7b926d113d2c09e9b102d206a2b542c11188dcbf
Piousangel <yyh7654@gmail.com>
2021-08-01 17:16:53 +0900

    Create 주식가격.java
    스택으로도 풀어보자...

---
+++ 프로그래머스2단계/주식가격.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Stack;
+
+class 주식가격 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        int[] count = new int[prices.length];
+        
+        for(int i=0; i < prices.length; i++){
+            for(int j=i+1; j < prices.length; j++){
+                count[i]++;
+                
+                if(prices[i] > prices[j]){
+                    break;
+                }
+            }
+        }
+        
+        for(int i=0; i < count.length; i++){
+            answer[i] = count[i];
+        }
+        return answer;
+    }
+}
