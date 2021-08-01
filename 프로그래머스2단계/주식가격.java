f56207cee1e6156590acb0f90283f2945fca9ebd
Piousangel <yyh7654@gmail.com>
2021-08-01 17:17:30 +0900

    Update 주식가격.java
    코드 간결화

--- 프로그래머스2단계/주식가격.java
+++ 프로그래머스2단계/주식가격.java
@@ -5,21 +5,21 @@ import java.util.Stack;
 class 주식가격 {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        int[] count = new int[prices.length];
+        //int[] count = new int[prices.length];
         
         for(int i=0; i < prices.length; i++){
             for(int j=i+1; j < prices.length; j++){
-                count[i]++;
+                answer[i]++;
+                //count[i]++;
                 
                 if(prices[i] > prices[j]){
                     break;
                 }
             }
         }
-        
-        for(int i=0; i < count.length; i++){
-            answer[i] = count[i];
-        }
+        // for(int i=0; i < count.length; i++){
+        //     answer[i] = count[i];
+        // }
         return answer;
     }
 }
