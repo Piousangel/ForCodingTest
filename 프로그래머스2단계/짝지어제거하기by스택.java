77d27c963cecd977f2caa4e326eaef62a5bbaeba
Piousangel <yyh7654@gmail.com>
2021-08-04 17:41:12 +0900

    스택복습...8/4

--- 프로그래머스2단계/짝지어제거하기by스택.java
+++ 프로그래머스2단계/짝지어제거하기by스택.java
@@ -9,34 +9,32 @@ class 짝지어제거하기by스택
 {
     public int solution(String s)
     {
+        Stack<String> st = new Stack<>();
         int answer = -1;
 
-        Stack<String> st = new Stack();
-        
         for(int i=0; i < s.length(); i++){
             if(st.isEmpty()){
                 st.push(String.valueOf(s.charAt(i)));
             }
             else{
-                String lastSt = st.peek();
+                String peekSt = st.peek();
                 String nowSt = String.valueOf(s.charAt(i));
                 
-                if(!lastSt.equals(nowSt)){
-                    st.push(nowSt);
+                if(peekSt.equals(nowSt)){
+                    st.pop();
                 }
                 else{
-                    st.pop();
+                    st.push(nowSt);
                 }
             }
             
-            if(st.size() != 0){
-                answer = 0;
+            if(st.isEmpty()){
+                answer = 1;
             }
             else
-                answer = 1;
+                answer = 0;
         }
         
-
         return answer;
     }
 }
