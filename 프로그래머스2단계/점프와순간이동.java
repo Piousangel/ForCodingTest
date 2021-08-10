59a4a531017868dac20220545eaf57fdee24dc91
Piousangel <yyh7654@gmail.com>
2021-08-10 19:11:44 +0900

    문제의도파악하기..!

--- 프로그래머스2단계/점프와순간이동.java
+++ 프로그래머스2단계/점프와순간이동.java
@@ -1,4 +1,19 @@
+import java.util.*;
 public class 점프와순간이동 {
     
-    //2018 summer/winter coding
+//2018 summer/winter coding
+public int solution(int n) {
+    int ans = 0;
+    
+    while(n != 0){
+        if(n %2 == 0){
+            n = n/2; 
+        }
+        else{
+            n--;
+            ans++;
+        }
+    }
+    return ans;
+}
 }
