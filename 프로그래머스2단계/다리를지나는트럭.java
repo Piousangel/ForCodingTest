e2b44b4809aa97f74a6ff86d8fdb391058868ac3
Piousangel <yyh7654@gmail.com>
2021-08-17 14:34:47 +0900

    다리를지나는트럭 오류나네..

--- 프로그래머스2단계/다리를지나는트럭.java
+++ 프로그래머스2단계/다리를지나는트럭.java
@@ -1,5 +1,31 @@
-public class 다리를지나는트럭 {
-    
-//by Queue
+import java.util.*;
 
+class 다리를지나는트럭 {
+    Queue<Integer> q;
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        q = new LinkedList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+        int cnt = 0;
+        int sum = 0;
+        int total_time = 0;
+        
+        for(int i =0; i < truck_weights.length; i++){
+            q.offer(truck_weights[i]);
+        }
+        
+        while(!q.isEmpty()){
+            if(sum < weight && arr.size() < bridge_length){
+                sum += q.peek();
+                arr.add(q.poll());
+                cnt++;
+            }
+            if(arr.size() == bridge_length || cnt == bridge_length){
+                total_time += cnt;
+                cnt = 0;
+            }
+        }
+        
+        int answer = 0;
+        return answer;
+    }
 }
