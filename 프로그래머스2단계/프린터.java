8c9745706ccc00112b0068cca549b01b08893e6e
Piousangel <yyh7654@gmail.com>
2021-08-17 14:08:15 +0900

    8월 17일 프린터 복습

--- 프로그래머스2단계/프린터.java
+++ 프로그래머스2단계/프린터.java
@@ -1,38 +1,43 @@
+import java.util.*;
+
 class 프린터 {
-    
-    //프린터 다시 풀다말음..
-    //Arrays.sort()는 Primitvie type Object type array를 정렬할 때 사용 한다.
-    //Collections.sort()는 Collection의 List를 정렬할때 사용
-    
-    class Pair{
-        int value;
-        int index;
-        Pair(int value, int index){
-            this.value = value;
-            this.index = index;
-        }
-    }
-    
-    Queue<Pair> q;
+    int answer = 0;
+    Queue<Printer> q;
     public int solution(int[] priorities, int location) {
-        q = new LinkedList<Pair>();
+        q = new LinkedList<>();
         
         for(int i=0; i < priorities.length; i++){
-            q.offer(new Pair(priorites[i], i));
+            q.offer(new Printer(i, priorities[i]));
         }
         
         while(!q.isEmpty()){
-            Pair p = q.poll();
-            int count = 0;
-            for(int i = q.peek(); i < priorities.length; i++){
-                
-                if(p.index && p.index == location){
-                    return count;
+            int tmp = q.peek().prior;
+            boolean chk = false;
+            for(Printer p : q){
+                if(tmp < p.prior){
+                    chk = true;
                 }
             }
+            if(chk){
+                q.offer(q.poll());           
+            }
+            else{
+                if(q.poll().position == location){
+                    answer = priorities.length - q.size();
+                }
+            }
+            
+            }
+            return answer;
         }
+    
+    public class Printer{
+        int position;
+        int prior;
         
-        int answer = count;
-        return answer;
+        public Printer(int position, int prior){
+            this.position = position;
+            this.prior = prior;
+        }
     }
 }
