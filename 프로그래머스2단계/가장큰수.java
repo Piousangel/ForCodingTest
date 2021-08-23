ac8899a61bc158553f0caeb7b9abd75f252b68d4
Piousangel <yyh7654@gmail.com>
2021-08-23 11:08:09 +0900

    8/23 compareTo 기억합시당

--- 프로그래머스2단계/가장큰수.java
+++ 프로그래머스2단계/가장큰수.java
@@ -1,39 +1,40 @@
 import java.util.*;
-import java.io.*;
 
-class Solution {
-    
+
     //맨앞에 0이면 배열길이 1 -> 아니면 0이 맨앞에 못오잖아?
     //배열 돌리면서 첫째자리 가장큰거 -> 같으면 다음자리 비교 -> 다음자리 비교
     //toString으로 바꿔서 출력하면 되나여?
-    
+    //equals , compareTo
+    //배열 정렬 -> Arrays.sort , ArrayList정렬 -> Collections.sort 안까먹지?
+
+class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        int max = 0;
-        int second = 0;
-        String max1 = "";
-        String[] k;
         
+        StringBuilder str = new StringBuilder();
+        String str2 = "";
         ArrayList<String> arr = new ArrayList<>();
-        ArrayList<Integer> Inarr = new ArrayList<>();
         
-        for(int i = 0; i < numbers.length; i++){
+        for(int i=0; i < numbers.length; i++){
             arr.add(Integer.toString(numbers[i]));
         }
         
-        for(int i = 0; i < arr.size(); i++){
-            k = new String[arr.size()];
-            k[i] = arr.get(i).substring(0,1);
-            Inarr.add(Integer.parseInt(k[i]));
-            //System.out.println(Inarr.get(i));
+        Comparator com = new Comparator<String>(){
+            @Override
+            public int compare(String a, String b){
+                return (b+a).compareTo(a+b);
+            }
+        };
+        
+        Collections.sort(arr, com);
+        
+        for(int i=0; i < arr.size(); i++){
+            str.append(arr.get(i));
         }
         
-        Collections.sort(Inarr);
+        if(arr.get(0).equals("0")){
+            return "0";
+        }
         
-        
-        
-        
-        
-        return answer;
+        return str.toString();
     }
 }
