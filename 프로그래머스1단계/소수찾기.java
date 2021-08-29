2a3f3b1a9573e00c8e83615c1b38db258346c615
Piousangel <yyh7654@gmail.com>
2021-08-29 10:41:09 +0900

    8/29

--- 프로그래머스1단계/소수찾기.java
+++ 프로그래머스1단계/소수찾기.java
@@ -5,19 +5,19 @@ class 소수찾기 {
         int answer = 0;
         int[] arr = new int[n+1];
         
-        for(int i=2; i<=n; i++){
+        for(int i=2; i <=n; i++){
             arr[i] = i;
         }
         
-        for(int i=2; i<=n; i++){
+        for(int i=2; i <=n; i++){
             if(arr[i] == 0) continue;
             
-            for(int j=2*i; j <=n; j+=i){
+            for(int j=i*2; j<=n; j+=i){
                 arr[j] = 0;
             }
         }
         
-        for(int i=0; i < arr.length; i++){
+        for(int i=0; i< arr.length; i++){
             if(arr[i] != 0){
                 answer++;
             }
