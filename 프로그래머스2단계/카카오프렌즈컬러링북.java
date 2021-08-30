452f1d564b66db8883306947bbc16dc50718ef92
Piousangel <yyh7654@gmail.com>
2021-08-30 20:44:31 +0900

    문자열 압축 좀더생각하기 일딴집감 8/30

--- 프로그래머스2단계/카카오프렌즈컬러링북.java
+++ 프로그래머스2단계/카카오프렌즈컬러링북.java
@@ -1,3 +1,68 @@
+import java.util*;
+
+// 8/30 복습하고 기록합시당
 public class 카카오프렌즈컬러링북 {
+    int[] dy = {-1,0,1,0};
+    int[] dx = {0,1,0,-1};
+    int size;
+    boolean[][] visited;
+    Queue<Node> q;
     
+    static public class Node{
+        int x;
+        int y;
+    
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
+
+        }
+    }
+    
+    
+    
+    public int[] solution(int m, int n, int[][] picture) {
+        visited = new boolean[m][n];
+        int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+        
+        for(int i=0; i < m ; i++){
+            for(int j=0; j < n ; j++){
+                if(!visited[i][j] && picture[i][j] > 0){
+                    size = 1;
+                    bfs(i,j,picture, m ,n);
+                    numberOfArea++;
+                    if(maxSizeOfOneArea < size){
+                        maxSizeOfOneArea = size;
+                    }
+                }
+            }
+        }
+        
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
+        return answer;
+    }
+    
+    public void bfs(int x, int y, int[][] pic, int m, int n){
+        q = new LinkedList<>();
+        q.offer(new Node(x,y));
+        visited[x][y] = true;
+        
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            for(int i=0; i < 4; i++){
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+                if(nx >=0 && ny >= 0 && nx < n && ny < m){
+                    if(!visited[nx][ny] && pic[x][y] == pic[nx][ny]){
+                        visited[nx][ny] = true;
+                        size++;
+                        q.offer(new Node(nx,ny));
+                    }
+                }
+            }
+        }
+    }
 }
