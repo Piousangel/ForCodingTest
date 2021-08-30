1bdd11ff5b62a72ba0ae35887fe70d469050a3fb
Piousangel <yyh7654@gmail.com>
2021-08-30 20:42:26 +0900

    문자열 압축 생각좀만 더해보자 일딴 집감 8/30

---
+++ 프로그래머스2단계/문자열압축.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+//음... 전체를 돌려야하나요
+//어째뜬 줄인 문자열 중에 가장 짧은 문자열을 찾아야 합니다...
+//생각 좀만더 해보자 8/30
+
+class 문자열압축 {
+    public int solution(String s) {
+        int answer = 0;
+        
+        for(int i=0; i < s.length(); i++){
+            int temp = doIt(s[i]);
+        }
+        
+
+        return answer;
+    }
+    
+    public int doIt(char c){
+        String str = "";
+        
+    }
+}
