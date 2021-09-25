8bd19c22a0d44469f3f79e0d325e02b8aeef3f32
Piousangel <yyh7654@gmail.com>
2021-09-25 22:14:55 +0900

    9/25

--- 프로그래머스1단계/폿켓몬.java
+++ 프로그래머스1단계/폿켓몬.java
@@ -1,28 +1,86 @@
-public import java.util.*;
+import java.util.*;
+
+// 9/25 이게 어떻게 1단계야.....
 
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int cnt = 0;
-        int chk = nums.length/2; //고르는 숫자
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++){
-            if(!list.contains(nums[i])){
-                list.add(nums[i]);
-            }
-            else
-                cnt++;
+        int count = 0;
+        int len = nums.length/2;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i < nums.length; i++){
+            map.put(Integer.toString(nums[i]), map.getOrDefault(Integer.toString(nums[i]),0)+1);   
         }
         
-        answer = nums.length - cnt;
+        Integer maxValue = Collections.max(map.values());
+        System.out.println("maxkey = " + maxValue);
         
-        // for(int i=0; i < list.size(); i++){
-        //     System.out.println(list.get(i));
-        // }
-        
-        return answer;
        
+        Iterator<String> keys = map.keySet().iterator();
+        int cnt = 0;
+        while( keys.hasNext() ){
+            String key = keys.next();
+            System.out.println( String.format("키 : %s, 값 : %s", key, map.get(key)) );
+            cnt++;
+        }
+        
+     
+        return answer;
     }
-}class 폿켓몬 {
-    
 }
+
+
+// class Solution {
+//     public int solution(int[] nums) {
+//         int answer = 0;
+//         int cnt = 0;
+//         int chk = nums.length/2; //고르는 숫자
+//         ArrayList<Integer> list = new ArrayList<>();
+//         for(int i = 0; i < nums.length; i++){
+//             if(!list.contains(nums[i])){
+//                 list.add(nums[i]);
+//             }
+//             else
+//                 cnt++;
+//         }
+        
+//         answer = nums.length - cnt;
+        
+//         // for(int i=0; i < list.size(); i++){
+//         //     System.out.println(list.get(i));
+//         // }
+        
+//         return answer;
+       
+//     }
+// }
+
+
+// class Solution {
+//     public int solution(int[] nums) {
+//         int answer = 0;
+//         int cnt = 0;
+//         int count = 1;
+//         int chk = nums.length/2; //고르는 숫자
+//         //ArrayList<Integer> list = new ArrayList<>();
+//         HashMap<String, Integer> map = new HashMap<>();
+//         for(int i = 0; i < nums.length; i++){
+//             if(!map.containsKey(Integer.toString(nums[i]))){
+//                 map.put(Integer.toString(nums[i]), count);
+//             }
+//             else
+//                 map.put(Integer.toString(nums[i]), count+1);
+//         }
+        
+//         Iterator<String> keys = map.keySet().iterator();
+        
+//         while( keys.hasNext() ){
+//             String key = keys.next();
+//             System.out.println( String.format("키 : %s, 값 : %s", key, map.get(key)) );
+//         }
+        
+//         return answer;
+       
+//     }
+// }
