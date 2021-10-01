b7cb436097a9ae5aac99071da2102407befd01a0
Piousangel <yyh7654@gmail.com>
2021-10-01 18:24:16 +0900

    10/01

---
+++ 프로그래머스3단계.../하노이의탑.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class 하노이의탑 {
+    
+    // 9/30 재귀함수
+
+    ArrayList<int[]> seq;
+    public int[][] solution(int n) {
+        seq = new ArrayList<>();
+        
+        hanoi(n, 1, 3, 2);
+        
+        int[][] answer = new int[seq.size()][2];
+        for(int i = 0 ; i < seq.size() ; ++i){
+            int[] cmd = seq.get(i);
+            answer[i][0] = cmd[0];
+            answer[i][1] = cmd[1];  
+        }
+        
+        return answer;
+    }
+    
+    private void hanoi(int n, int from, int to, int via){
+        int[] move = {from, to};
+        
+        if(n == 1) {
+            seq.add(move);
+        } else {
+            hanoi(n - 1, from, via, to);
+            seq.add(move);
+            hanoi(n - 1, via, to, from);   
+        }
+    }
+}
