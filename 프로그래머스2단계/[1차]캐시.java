6a1eb4be0f153da6efdd7de0c4bd91643e77bba7
Piousangel <yyh7654@gmail.com>
2021-10-03 19:36:58 +0900

    10/03

--- 프로그래머스2단계/[1차]캐시.java
+++ 프로그래머스2단계/[1차]캐시.java
@@ -1,52 +1,93 @@
 import java.util.*;
 
-// 10/03
+// 10/03 풀읏다..
+import java.util.*;
 
 class Solution {
-    
-    public class Cache{
-        String name;
-        int time;
-        
-        public Cache(String name, int time){
-            this.name = name;
-            this.time = time;
-        }
-    }
-    
-    Queue<Cache> q;
-    
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
+        
+        LinkedList<String> list = new LinkedList<>();
         int cnt = 0;
-        q = new LinkedList<>();
-        
-        String[] str = new String[cacheSize]; // 캐시 크기만큼의 스트링 배열 만듬
-        
-        for(int i=0; i < cities.length; i++){
-            q.offer(new Cache(cities[i],0));  //큐에 일딴 이름,0으로 다넣어줌
+        if(cacheSize == 0){
+            return cities.length*5;
         }
         
-        while(!q.isEmpty()){
-            Cache ca = q.peek();
+        for(int i = 0; i < cities.length; i++){
+            cities[i] = cities[i].toUpperCase(); // 대소문자 구분X
             
-            for(int i=0; i < str.length; i++){
-                if(!ca.name.equals(str[i])){
-                    cnt++;
-                    str[i] = ca.name;
-                    q.poll();
+            if(!list.contains(cities[i])){    //miss
+                if(list.size() < cacheSize){
+                    list.add(cities[i]);
+                    cnt += 5;
                 }
                 else{
-                    (Cache c : String name){
-                        Math.min(c.time.value)//캐쉬 가장 작은 값을 찾아 그것의 위치와 바꿔줌
-                        cnt += 5;
-                    }
+                    list.remove(0);
+                    list.add(cities[i]);
+                    cnt += 5;
                 }
+            }else{                          //no miss
+                list.remove(cities[i]);
+                list.add(cities[i]);
+                cnt += 1;
             }
-            
         }
         
-        
-        return answer;
+        return cnt;
     }
 }
+// class Solution {
+    
+//     public class Cache{
+//         String name;
+//         int time;
+        
+//         public Cache(String name,int time){
+//             this.name = name;
+//             this.time = time;
+//         }
+//     }
+    
+//     Queue<Cache> q;
+    
+//     public int solution(int cacheSize, String[] cities) {
+//         int answer = 0;
+//         q = new LinkedList<>();
+//         ArrayList<String> list = new ArrayList<>();
+//         //int[] cnt = new int[cacheSize];
+//         int cnt = 0;
+        
+//         for(int i=0; i < cities.length; i++){
+//             q.offer(new Cache(cities[i],0));
+//         }
+        
+//         while(!q.isEmpty()){
+//             if(list.size() < cacheSize){
+//                 list.add(q.peek().name);
+//                 q.peek().time++;
+//                 cnt += 5;
+//             }
+//             else{
+//                 for(int j=0; j < list.size(); j++){
+//                     if(q.peek().name == list.get(i)){
+//                         q.peek().time = 0;
+//                         cnt += 1;
+//                     }
+//                     else{
+//                         for(Cache c : q){
+//                             Math.min(c.time);
+//                             // c.time 이 제일 작은값 가져와서
+//                             list.set(c.name , q.peek().name);
+//                             q.peek().time = 0;
+//                             cnt += 5;
+//                         }
+//                     }
+//                 }
+//             }
+//             q.poll();
+            
+//         }
+        
+//         return cnt;
+//     }
+// }
---
+++ 프로그래머스2단계/후보키.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+//10/03
+
+class Solution {
+    public int solution(String[][] relation) {
+        int answer = 0;
+        return answer;
+    }
+}
