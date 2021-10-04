b0fe758e86ed50f6f45a2c887e8bae1e87e9e0a0
Piousangel <yyh7654@gmail.com>
2021-10-04 15:48:56 +0900

    10/04

---
+++ 조합,순열,BFS,DFS,Graph,DP,Brute Force/조합/조합/조합(conbin/조합(Combination)/Combination(Backtracking).java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+//조합이란 n개의 숫자 중에서 r개의 수를 순서 없이 뽑는 경우를 말한다.
+
+public class Combination {
+
+    public static void main(String[] args) {
+        int n = 4;
+        int[] arr = {1, 2, 3, 4};
+        boolean[] visited = new boolean[n];
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("\n" + n + " 개 중에서 " + i + " 개 뽑기");
+            combination(arr, visited, 0, n, i);
+        }
+    }
+
+    static void combination(int[] arr, boolean[] visited, int start, int n, int r) {
+        if (r == 0) {
+            print(arr, visited, n);
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            visited[i] = true;
+            combination(arr, visited, i + 1, n, r - 1);
+            visited[i] = false;
+        }
+    }
+}
--- 프로그래머스2단계/후보키.java
+++ 프로그래머스2단계/후보키.java
@@ -3,8 +3,67 @@ import java.util.*;
 //10/03
 
 class Solution {
+
+    public static ArrayList<String> list = new ArrayList<>();
+    public static ArrayList<List<String>> candidateKeys = new ArrayList<>();
+
     public int solution(String[][] relation) {
-        int answer = 0;
-        return answer;
+        int tuple = relation.length;    //행 수
+        int column = relation[0].length; //열 수
+        boolean[] visited = new boolean[column];
+
+        for(int i = 1; i <= column; i++){   //1,2,3,4,5
+            comb(visited, 0, i);
+        }
+
+        for(int i=0; i < list.size(); i++){
+            HashSet<String> set = new HashSet<>();
+            String[] keys = list.get(i).split("");
+
+            for(int j=0; j < relation.length; j++){
+                String r = "";
+                for(int k=0; k < keys.length; k++){
+                    r += relation[j][Integer.parseInt(keys[k])];
+                }
+                set.add(r);
+            }
+
+            if(set.size() == tuple){
+                List<String> cKey = Arrays.asList(list.get(i).split(""));
+    
+                boolean flag = true;
+                for(int j=0; j < candidateKeys.size(); j++){
+                    if(cKey.containsAll(candidateKeys.get(j))){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    candidateKeys.add(cKey);
+                }
+            }
+        }
+        return candidateKeys.size();
+    }
+
+    public static void comb(boolean[] visited, int start, int r){ 
+        //처음 (false, 0, 1);
+        if(r == 0){
+            String num = "";
+            for(int i=0; i < visited.length; i++){
+                if(visited[i]){
+                    num = num + i;
+                }
+            }
+            list.add(num);
+            return;
+        }
+        else{
+            for(int i= start; i < visited.length; i++){
+                visited[i] = true;
+                comb(visited, i+1, r-1);
+                visited[i] = false;
+            }
+        }
     }
 }
