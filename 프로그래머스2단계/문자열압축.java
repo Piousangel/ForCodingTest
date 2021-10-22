e387c608b70ec0e50c27689f02cd1151f2cc8e7b
Piousangel <yyh7654@gmail.com>
2021-10-22 20:44:45 +0900

    10/22

--- 프로그래머스2단계/문자열압축.java
+++ 프로그래머스2단계/문자열압축.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 //늘려나갈 글자수 -> 다음 글자수가 같은지 비교
-//결과값       10/10
+//결과값       10/10  10/22 q;lwhflqwel ㅡ.ㅡ 안풀러ㅕㅓㅕㅕㅕ
 
 class Solution {
     int len = 0;
@@ -9,8 +9,13 @@ class Solution {
         int answer = 0;
         int cnt = 1000;
         int tmp = 0;
+
+        int maxCut = 0;
         
-        for(int i=1; i <= s.length(); i++){
+        if(s.length() % 2 == 0) maxCut = s.length()/2;
+        else maxCut = (s.length()-1) /2;
+        
+        for(int i=1; i <= maxCut; i++){
             len = lenCnt(i,s);  //몇개씩 커팅할지
             tmp = Math.min(cnt, len);
         }
@@ -22,7 +27,7 @@ class Solution {
         int cnt = s.length();
         //String last = s.subString(cut, s.length);
         
-        for(int i=0; i < s.length(); i++){           //자를 때 범위 수정해야함... 예를들어 앞에서 10개 잘랐는데 뒤에 10개가 없으면 못잘라요...
+        for(int i=0; i < maxCut; i++){           //자를 때 범위 수정해야함... 예를들어 앞에서 10개 잘랐는데 뒤에 10개가 없으면 못잘라요...
             String str = s.substring(i,i+cut);
             if(str.equals(s.substring(i+cut,i+cut+cut)) && str.length() <= cnt/2 ){
                 cnt = cnt - 1;
@@ -32,3 +37,39 @@ class Solution {
     }
     
 }
+
+
+
+// class Solution {
+//     public int solution(String s) {
+//         int answer = 0;
+        
+//         int maxCut = 0;
+        
+//         if(s.length() % 2 == 0) maxCut = s.length()/2;
+//         else maxCut = (s.length()-1) /2;
+        
+//         for(int i=1; i < maxCut; i++){
+            
+//             String str1 = s.substring(0,i);
+//             String str2 = s.substring(i,s.length());
+//             reTry(str1, str2);
+//             System.out.print(str1+ " ");
+//             System.out.println(str2);
+//         }
+        
+//         return answer;
+//     }
+    
+//     public int reTry(String str1, String str2){
+//         int cnt = 0;
+//         String str3 = str2.substring(0,str1.length());  // 똑같이 자름
+//         String str4 = str2.substring(str1.length(), str2.length());
+//         if(str1.equals(str3)){
+//             reTry(str3,str4);
+//         }
+//         else{
+            
+//         }
+//     }
+// }
--- 프로그래머스2단계/쿼드압축후세기.java
+++ 프로그래머스2단계/쿼드압축후세기.java
@@ -1,6 +1,85 @@
 import java.util.*;
 
-// 10/13
-public class 쿼드압축후세기 {
+//10/22 다시.....ㅇㄴ.....
+
+class Solution {
     
+    int[] dx = {0,0,1,-1};
+    int[] dy = {1,-1,0,0};
+    
+    public class Node{
+        int x;
+        int y;
+        
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    Queue<Node> q;
+    int m;
+    int n;
+    int zeroArea = 0;
+    int oneArea = 0;
+    
+    boolean[][] visited;
+    public int[] solution(int[][] arr) {
+        
+        int[] answer = new int[2];
+        m = arr.length;
+        n = arr[0].length;
+        visited = new boolean[m][n];
+        zeroArea = 0;
+        oneArea = 0;
+        
+        for(int i =0; i < m; i++){
+            for(int j=0; j < n; j++){
+                if(visited[i][j] != true){
+                    bfs(arr,i,j,m,n);
+                }
+            }
+        }
+        
+        answer[0] = zeroArea;
+        answer[1] = oneArea;
+        return answer;
+    }
+    
+    public void bfs(int[][] arr, int x, int y, int m, int n){
+        q = new LinkedList<>();
+        q.offer(new Node(x,y));
+        visited[x][y] = true;
+        
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            
+            for(int i=0; i < 4; i++){
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+                
+                if(nx >=0 && nx < m && ny >=0 && ny < n){
+                    if(visited[nx][ny] || arr[nx][ny] != arr[x][y]){
+                        if(arr[x][y] ==1){
+                            oneArea++;
+                        }
+                        else zeroArea++;
+                        break;
+                    }
+                    else{
+                        if(arr[x][y] == 1){
+                            oneArea++;
+                        }
+                        else zeroArea++;
+            
+                        visited[nx][ny] = true;
+                        q.offer(new Node(nx,ny));
+                        
+                    }
+                }
+            }
+        }
+        
+        
+    }
 }
