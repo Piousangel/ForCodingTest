48fe20f7c3d7aa4a2957cde1f617709d18785574
Piousangel <yyh7654@gmail.com>
2021-10-23 11:51:11 +0900

    10/23

---
+++ 프로그래머스2단계/이진탐색by재귀.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+//재귀돌릴꺼임 이진탐!
+class Solution {
+    public int solution(int[] sortedArray, int findValue) {
+        
+        int answer = binarySearch(sortedArray, findValue, 0, sortedArray.length-1); //찾는수 index 0, 끝
+        return answer;
+    }
+    
+    public int binarySearch(int[] arr, int key, int start, int end){
+        int mid;
+        
+        if(start <= end){
+            mid = (start + end) /2;   //중간점
+            
+            if(key == arr[mid]){  //찾음
+                return mid;
+            }
+            else if(key < arr[mid]){
+                return binarySearch(arr, key, start, mid-1);
+            }
+            else{
+                return binarySearch(arr, key, mid+1, end);
+            }
+        }
+        
+        return -1;  //못찾음
+    }
+}
---
+++ 프로그래머스2단계/카페24문제1번.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+// 1~9 2개
+//ex 1 -> 0000000001 이런식
+class Solution {
+    public String[] solution(int startNumber, int endNumber) {
+             
+        ArrayList<String> list = new ArrayList<>();
+               
+        int zeroLen = 0;
+        
+        if(endNumber > startNumber){   // 뒤에 숫자가 클때
+            String str = "";
+            for(int i = startNumber; i <= endNumber; i++){    //2,3,4,5
+                StringBuilder sb = new StringBuilder();
+                str += Integer.toString(i);
+                zeroLen = 10 - str.length();
+                for(int j=0; j < zeroLen; j++){
+                    sb.append("0");                   
+                }
+                sb.append(str);
+                list.add(sb.toString());
+            }
+            //System.out.println(sb);      
+        }
+        else{                 //앞에숫자클때
+            String str = "";
+            for(int i= startNumber; i >= endNumber; i--){
+                StringBuilder sb = new StringBuilder();
+                str += Integer.toString(i);
+                zeroLen = 10 - str.length();
+                for(int j=0; j < zeroLen; j++){
+                    sb.append("0");                   
+                }
+                sb.append(str);
+                list.add(sb.toString());
+            }
+        }
+        
+        String[] answer = new String[list.size()];
+        
+        for(int i=0; i < list.size(); i++){
+            answer[i] = "";          //null값 제거
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
