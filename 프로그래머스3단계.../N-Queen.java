0df6e2aff6bbd7365c1a55c775c4129efdf70df4
Piousangel <yyh7654@gmail.com>
2021-11-02 20:39:47 +0900

    11/2

--- 프로그래머스3단계.../N-Queen.java
+++ 프로그래머스3단계.../N-Queen.java
@@ -2,43 +2,43 @@ import java.util.*;
 
 class Solution {
     
-    static int[] col_len;
-    static int answer;
+    int[] col_len;
+    int answer;
     
     public int solution(int n) {
         answer = 0;
         
         for(int i = 0 ; i < n ; ++i) {
             col_len = new int[n];
-            col_len[0] = i;
+            col_len[0] = i;  //0~n;
             backtracking(n, 1);
         }
         
         return answer;
     }
     
-    private void backtracking(int max, int row_len){
+    public void backtracking(int max, int row_len){
         if(row_len == max){
             answer++;
-            col_len[row_len - 1] = 0;
+            col_len[row_len - 1] = 0;  //0으로 초기화
             return;
         }
         
         for(int i = 0 ; i < max ; ++i){
             col_len[row_len] = i;
             if(isPossible(row_len)){
-                backtracking(max, row_len + 1);
+                backtracking(max, row_len + 1);   //n, ++
             } else {
-                col_len[row_len] = 0;
+                col_len[row_len] = 0;      
             }
         }
         col_len[row_len] = 0;
     }
     
-    private boolean isPossible(int row_len){
+    public boolean isPossible(int row_len){
         for(int i = 0 ; i < row_len ; ++i){
             if(col_len[i] == col_len[row_len]) return false;
-            if(Math.abs(row_len - i) == Math.abs(col_len[row_len] - col_len[i])) return false;
+            if(Math.abs(row_len - i) == Math.abs(col_len[row_len] - col_len[i])) return false;  //조건
         }
         
         return true;
