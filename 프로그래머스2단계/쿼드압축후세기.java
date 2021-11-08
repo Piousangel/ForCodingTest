e9a1e208d4953fffa7a0493e43c90b7b3a786926
Piousangel <yyh7654@gmail.com>
2021-11-08 13:00:25 +0900

    11/8

--- 프로그래머스2단계/쿼드압축후세기.java
+++ 프로그래머스2단계/쿼드압축후세기.java
@@ -1,85 +1,127 @@
 import java.util.*;
 
 //10/22 다시.....ㅇㄴ.....
+// 11/8
 
 class Solution {
     
-    int[] dx = {0,0,1,-1};
-    int[] dy = {1,-1,0,0};
+    int[] answer = new int[2];
     
-    public class Node{
-        int x;
-        int y;
-        
-        public Node(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
-    Queue<Node> q;
-    int m;
-    int n;
-    int zeroArea = 0;
-    int oneArea = 0;
-    
-    boolean[][] visited;
     public int[] solution(int[][] arr) {
         
-        int[] answer = new int[2];
-        m = arr.length;
-        n = arr[0].length;
-        visited = new boolean[m][n];
-        zeroArea = 0;
-        oneArea = 0;
-        
-        for(int i =0; i < m; i++){
-            for(int j=0; j < n; j++){
-                if(visited[i][j] != true){
-                    bfs(arr,i,j,m,n);
-                }
-            }
-        }
-        
-        answer[0] = zeroArea;
-        answer[1] = oneArea;
+        dfs(arr.length, 0, 0, arr);
         return answer;
     }
     
-    public void bfs(int[][] arr, int x, int y, int m, int n){
-        q = new LinkedList<>();
-        q.offer(new Node(x,y));
-        visited[x][y] = true;
+    public void dfs(int len, int row, int col, int[][] arr){
         
-        while(!q.isEmpty()){
-            Node node = q.poll();
+        if(len == 1){
+            answer[arr[row][col]]++;
+            return;
+        } 
+        
+        if(isBlock(len, arr, row, col)) return;  //true면 블럭이므로 리턴시켜
             
-            for(int i=0; i < 4; i++){
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                
-                if(nx >=0 && nx < m && ny >=0 && ny < n){
-                    if(visited[nx][ny] || arr[nx][ny] != arr[x][y]){
-                        if(arr[x][y] ==1){
-                            oneArea++;
-                        }
-                        else zeroArea++;
-                        break;
-                    }
-                    else{
-                        if(arr[x][y] == 1){
-                            oneArea++;
-                        }
-                        else zeroArea++;
-            
-                        visited[nx][ny] = true;
-                        q.offer(new Node(nx,ny));
-                        
-                    }
-                }
+        dfs(len/2, row, col, arr);       //2의n승이므로 dfs로 1이될때까지 계속 4등분
+        dfs(len/2, row+len/2, col, arr);
+        dfs(len/2, row, col+len/2, arr);
+        dfs(len/2, row+len/2, col+len/2, arr);
+    }
+    
+    public boolean isBlock(int len, int[][] arr, int row, int col){
+        
+        for(int i = row; i < row+len; i++){
+            for(int j = col; j < col+len; j++){
+                if(arr[i][j] != arr[row][col]) return false;
             }
         }
+        // if(arr[row][col] == 0)  answer[0]++;
+        // else answer[1]++;
+        answer[arr[row][col]]++;
         
-        
+        return true;
     }
 }
+
+
+// class Solution {
+    
+//     int[] dx = {0,0,1,-1};
+//     int[] dy = {1,-1,0,0};
+    
+//     public class Node{
+//         int x;
+//         int y;
+        
+//         public Node(int x, int y){
+//             this.x = x;
+//             this.y = y;
+//         }
+//     }
+    
+//     Queue<Node> q;
+//     int m;
+//     int n;
+//     int zeroArea = 0;
+//     int oneArea = 0;
+    
+//     boolean[][] visited;
+//     public int[] solution(int[][] arr) {
+        
+//         int[] answer = new int[2];
+//         m = arr.length;
+//         n = arr[0].length;
+//         visited = new boolean[m][n];
+//         zeroArea = 0;
+//         oneArea = 0;
+        
+//         for(int i =0; i < m; i++){
+//             for(int j=0; j < n; j++){
+//                 if(visited[i][j] != true){
+//                     bfs(arr,i,j,m,n);
+//                 }
+//             }
+//         }
+        
+//         answer[0] = zeroArea;
+//         answer[1] = oneArea;
+//         return answer;
+//     }
+    
+//     public void bfs(int[][] arr, int x, int y, int m, int n){
+//         q = new LinkedList<>();
+//         q.offer(new Node(x,y));
+//         visited[x][y] = true;
+        
+//         while(!q.isEmpty()){
+//             Node node = q.poll();
+            
+//             for(int i=0; i < 4; i++){
+//                 int nx = node.x + dx[i];
+//                 int ny = node.y + dy[i];
+                
+//                 if(nx >=0 && nx < m && ny >=0 && ny < n){
+//                     if(visited[nx][ny] || arr[nx][ny] != arr[x][y]){
+//                         if(arr[x][y] ==1){
+//                             oneArea++;
+//                         }
+//                         else zeroArea++;
+//                         break;
+//                     }
+//                     else{
+//                         if(arr[x][y] == 1){
+//                             oneArea++;
+//                         }
+//                         else zeroArea++;
+            
+//                         visited[nx][ny] = true;
+//                         q.offer(new Node(nx,ny));
+                        
+//                     }
+//                 }
+//             }
+//         }
+        
+        
+//     }
+// }
--- 프로그래머스3단계.../정수삼각형.java
+++ 프로그래머스3단계.../정수삼각형.java
@@ -1,48 +1,81 @@
-import java.util.*;
+// import java.util.*;
 
-// 이동시 대각선 방향 한칸 or 오른쪽 or 왼쪽으로만 이동가능 
-// 답은 되는데 시간초과가 납니다.. 이래서 DP써야되나봅니다... 11/7
-class Solution {
-    
-   int answer = 0;
-    
-    public int solution(int[][] triangle) {
+// class Solution {
+//     public int solution(int[][] triangle) {
+//         int answer = 0;
         
-        //int sum = triangle[0][0];
-        int[] ar = new int[triangle.length];
-         // 0 1 2 3 4 
-        boolean[][] visited = new boolean[triangle.length][triangle.length];
-        dfs(triangle, ar, visited, 0, 0);
+//         int[][] arr = new int[triangle.length][triangle.length];
         
-        return answer;
-    }
-    
-    public void dfs(int[][] triangle, int[] arr, boolean[][] visited, int row, int col){
+//         arr[0][0] = triangle[0][0];
         
-        if(row == arr.length){
-            print(arr);
-            return;
-        }
+//         for(int i=1; i < triangle.length; i++){
+//             for(int j=0; j < triangle[i].length; j++){
+//                 if(j==0){
+//                     arr[i][j] = arr[i-1][j] + triangle[i][j];
+//                 }
+//                 else if(j == triangle[i].length-1){
+//                     arr[i][j] = arr[i-1][j-1] + triangle[i][j];
+//                 }
+//                 else{
+//                     arr[i][j] = Math.max(arr[i-1][j], arr[i-1][j-1]) + triangle[i][j];
+//                 }
+//             }    
+//         }
         
-        for(int i=0; i < triangle[row].length; i++){   //열을 돌림
-            if(visited[row][i] != true && i - col == 0 || i - col == 1){
-                //System.out.print("Math.abs :" + Math.abs(i-col) + ", ");
-                visited[row][i] = true;
-                arr[row] = triangle[row][i];
-                dfs(triangle, arr, visited, row+1, i);
-                visited[row][i] = false;
-            }
-        }
-    }
+//         for(int i=0; i < arr.length; i++){
+//             answer = Math.max(answer, arr[arr.length-1][i]);
+//         }
+        
+//         return answer;
+//     }
+// }
+
+
+// import java.util.*;
+
+// // 이동시 대각선 방향 한칸 or 오른쪽 or 왼쪽으로만 이동가능 
+// // 답은 되는데 시간초과가 납니다.. 이래서 DP써야되나봅니다... 11/7
+// class Solution {
     
-    public void print(int[] arr){
-        int sum = 0;
-        for(int i=0; i < arr.length; i++){
-            sum += arr[i];
-            //System.out.print(arr[i] + " ");
-        }
+//    int answer = 0;
     
-        answer = Math.max(sum, answer);
-        //System.out.println();     
-    }
-}
+//     public int solution(int[][] triangle) {
+        
+//         //int sum = triangle[0][0];
+//         int[] ar = new int[triangle.length];
+//          // 0 1 2 3 4 
+//         boolean[][] visited = new boolean[triangle.length][triangle.length];
+//         dfs(triangle, ar, visited, 0, 0);
+        
+//         return answer;
+//     }
+    
+//     public void dfs(int[][] triangle, int[] arr, boolean[][] visited, int row, int col){
+        
+//         if(row == arr.length){
+//             print(arr);
+//             return;
+//         }
+        
+//         for(int i=0; i < triangle[row].length; i++){   //열을 돌림
+//             if(visited[row][i] != true && i - col == 0 || i - col == 1){
+//                 //System.out.print("Math.abs :" + Math.abs(i-col) + ", ");
+//                 visited[row][i] = true;
+//                 arr[row] = triangle[row][i];
+//                 dfs(triangle, arr, visited, row+1, i);
+//                 visited[row][i] = false;
+//             }
+//         }
+//     }
+    
+//     public void print(int[] arr){
+//         int sum = 0;
+//         for(int i=0; i < arr.length; i++){
+//             sum += arr[i];
+//             //System.out.print(arr[i] + " ");
+//         }
+    
+//         answer = Math.max(sum, answer);
+//         //System.out.println();     
+//     }
+// }
