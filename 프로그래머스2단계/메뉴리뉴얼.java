253cd71eb45cd7265a74884786ecb64991ba423a
Piousangel <yyh7654@gmail.com>
2021-11-09 22:39:01 +0900

    11/9

--- 프로그래머스2단계/메뉴리뉴얼.java
+++ 프로그래머스2단계/메뉴리뉴얼.java
@@ -1,82 +1,145 @@
 import java.util.*;
-import java.util.Map.Entry;
 
-//최소 2가지이상   //다시 다시다시 조합.. 10/27
+//String 쪼개서 char배열에 담아 -> 총~ 2,3,4,5 개 중 가장 많이 나온거 chk?
+
 
 class Solution {
     public String[] solution(String[] orders, int[] course) {
-       
-        Map<String, Integer> map = new HashMap<>();
-        ArrayList<String> answer = new ArrayList<>();
+        String[] answer = {};
         
-        for(int i=0; i < orders.length; i++){
-            int max = 0;
-            StringBuilder sb = new StringBuilder();
-            sb.append(orders[i]);
-            
-            
-            for(int j=0; j < orders.length; j++){
-                String str = sb.toString();
-                for(int k=0; k < course.length; k++){
-                    if(str.length() == course[k]){
-                        if(orders[j].contains(str)){
-                            if(!map.containsKey(str)){
-                                map.put(str,1);
-                            }
-                            else{
-                                map.put(str,map.get(str)+1);
-                            }
-                            
-                        }
-                    }
-                }
-                max = Math.max(max, map.get(str));
-            }
-            
-            for(Entry<String, Integer> entry : map.entrySet()){
-                if(max >= 2 && entry.getValue() == max){
-                    answer.add(entry.getKey());
+        for(int i=0; i < orders.length; i++){  // 손님들의 단품메뉴들
+            for(int j=0; j < course.length; j++){  // orders[i] 개중 course[j]개뽑기
+                if(orders[i].length() < course[j]) continue; //조합의 수가 더 크면 안댐
+                else{
+                    char[] ch = orders[i].toCharArray();  // 조합하려고 배열에 담음
+                    Arrays.sort(ch);
+                    char[] arr = new char[ch.length];  //배열에 담게
+                    boolean[] visited = new boolean[ch.length];
+                    dfs(ch,arr,visited, ch.length, course[j], 0);
                 }
             }
-            
-            
         }
         
         Iterator<String> iter = map.keySet().iterator();
         
         while(iter.hasNext()){
-            String key = iter.next();
-            
-            System.out.print("key :" + key);
-            System.out.println(" value :" + map.get(key));
+            String str = iter.next();
+            System.out.print("Key : " + str);
+            System.out.println(" Values : " + map.get(str) + " ");
         }
+        
+        return answer;
+    }
+    
+    public void dfs(char[] ch, char[] arr, boolean[] visited, int n, int r, int idx){
+        
+        if(idx == r){
+            print(arr);
+            return;
+        }
+        
+        for(int i=0; i < ch.length; i++){
+            if(visited[i] != true){
+                visited[i] = true;
+                arr[idx] = ch[i];
+                dfs(ch, arr, visited, n, r, idx+1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    Map<String, Integer> map = new HashMap<>();
+    
+    public void print(char[] arr){
+        String str = "";
+        for(int i=0; i < arr.length; i++){
+            str += arr[i]+"";
+        }
+        
+        if(!map.containsKey(str)){
+            map.put(str,1);
+        }
+        else map.put(str, map.get(str)+1);
+    }
+    
+}
+
+// import java.util.*;
+// import java.util.Map.Entry;
+
+// //최소 2가지이상   //다시 다시다시 조합.. 10/27
+
+// class Solution {
+//     public String[] solution(String[] orders, int[] course) {
+       
+//         Map<String, Integer> map = new HashMap<>();
+//         ArrayList<String> answer = new ArrayList<>();
+        
+//         for(int i=0; i < orders.length; i++){
+//             int max = 0;
+//             StringBuilder sb = new StringBuilder();
+//             sb.append(orders[i]);
+            
+            
+//             for(int j=0; j < orders.length; j++){
+//                 String str = sb.toString();
+//                 for(int k=0; k < course.length; k++){
+//                     if(str.length() == course[k]){
+//                         if(orders[j].contains(str)){
+//                             if(!map.containsKey(str)){
+//                                 map.put(str,1);
+//                             }
+//                             else{
+//                                 map.put(str,map.get(str)+1);
+//                             }
+                            
+//                         }
+//                     }
+//                 }
+//                 max = Math.max(max, map.get(str));
+//             }
+            
+//             for(Entry<String, Integer> entry : map.entrySet()){
+//                 if(max >= 2 && entry.getValue() == max){
+//                     answer.add(entry.getKey());
+//                 }
+//             }
+            
+            
+//         }
+        
+//         Iterator<String> iter = map.keySet().iterator();
+        
+//         while(iter.hasNext()){
+//             String key = iter.next();
+            
+//             System.out.print("key :" + key);
+//             System.out.println(" value :" + map.get(key));
+//         }
          
       
             
-//         for(Entry<String, Integer> entry : map.entrySet()){  
-//             max = Math.max(max, entry.getValue());
+// //         for(Entry<String, Integer> entry : map.entrySet()){  
+// //             max = Math.max(max, entry.getValue());
+// //         }
+        
+// //         for(Entry<String, Integer> entry : map.entrySet()){
+// //             if(max >= 2 && entry.getValue() == max){
+// //                 answer.add(entry.getKey());
+// //             }
+// //         }
+        
+        
+//         Collections.sort(answer);
+        
+//         String[] ans = new String[answer.size()];
+        
+//         for(int i=0; i < answer.size(); i++){
+//             ans[i] = answer.get(i);
 //         }
         
-//         for(Entry<String, Integer> entry : map.entrySet()){
-//             if(max >= 2 && entry.getValue() == max){
-//                 answer.add(entry.getKey());
-//             }
-//         }
         
         
-        Collections.sort(answer);
-        
-        String[] ans = new String[answer.size()];
-        
-        for(int i=0; i < answer.size(); i++){
-            ans[i] = answer.get(i);
-        }
-        
-        
-        
-        return ans;
-    }
-    
-    
-    
-}
+//         return ans;
+//     }  
+// }
--- 프로그래머스2단계/타겟넘버BFS.java
+++ 프로그래머스2단계/타겟넘버BFS.java
@@ -3,37 +3,39 @@ import java.util.*;
 //BFS를 이용한 풀이 
 //DFS로도 풀어바... 재귀사용 9/30
 
-class 타겟넘버 {
-    public int solution(int[] numbers, int target) {
+class Solution {
+    
+    public class Pair{
+        int cur;
+        int idx;
         
-        class Pair{
-            int cur;
-            int index;
-            
-            Pair(int cur, int index){
-                this.cur = cur;
-                this.index = index;
-            }
+        public Pair(int cur, int idx){
+            this.cur = cur;
+            this.idx = idx;
         }
+    }
+    
+    public int solution(int[] numbers, int target) {
         int answer = 0;
-        
         Queue<Pair> q = new LinkedList<Pair>();
         q.offer(new Pair(numbers[0],0));
         q.offer(new Pair(-numbers[0],0));
         
         while(!q.isEmpty()){
             Pair p = q.poll();
-            if(p.index == numbers.length-1){
+            if(p.idx == numbers.length-1){
                 if(p.cur == target){
                     answer = answer+1;
                 }
                 continue;
             }
-            int c1 = p.cur + numbers[p.index+1];
-            int c2 = p.cur - numbers[p.index+1];
+            int c1 = p.cur + numbers[p.idx+1];
+            int c2 = p.cur - numbers[p.idx+1];
             
-            q.offer(new Pair(c1, p.index+1));
-            q.offer(new Pair(c2, p.index+1));
+            q.offer(new Pair(c1, p.idx+1));
+            q.offer(new Pair(c2, p.idx+1));
+            
+            //System.out.println("q.size() : " + q.size());
         }
         
         return answer;
