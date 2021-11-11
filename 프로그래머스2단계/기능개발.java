202678abc86e5e0bd23ea6ed70b3e8c57f00e161
Piousangel <yyh7654@gmail.com>
2021-11-11 18:28:43 +0900

    11/11

--- 프로그래머스2단계/기능개발.java
+++ 프로그래머스2단계/기능개발.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 //8월1일 이후 Queue 복습후 8월2일  8월19일 8/23일 9/16
-//이문제에서 가장중요한 부분은 poll()을 기가막힌 타이밍에 하기위해 큐를 쓰는 것이다... 라고 하겠음
-
+// ㅡㅡ 
 class 기능개발 {
     Queue<Integer> q;
     
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
+
         ArrayList<Integer> Alist = new ArrayList<>();
         q = new LinkedList<>();
         
@@ -21,16 +20,17 @@ class 기능개발 {
         while(!q.isEmpty()){
             for(int i= q.peek(); i < speeds.length; i++){
                 progresses[i] += speeds[i];
+
+                int insert = Qpoll(progresses);
+        
+                if(insert > 0){
+                    Alist.add(insert);
+                }
             }
             
-            int insert = Qpoll(progresses);
-        
-            if(insert > 0){
-                Alist.add(insert);
-            }
         }
 
-        answer = new int[Alist.size()];
+        int[] answer = new int[Alist.size()];
         
         for(int i=0; i < Alist.size(); i++){
             answer[i] = Alist.get(i);
@@ -49,3 +49,68 @@ class 기능개발 {
     }
 }
 
+// // 뒤 기능이 먼저 완성되도 앞기능이 완성 안되었으면 기다림
+
+// class Solution {
+    
+//     public class Info{
+//         int pro;
+//         int sp;
+        
+//         public Info(int pro, int sp){
+//             this.pro = pro;
+//             this.sp = sp;
+//         }
+//     }
+    
+//     Queue<Info> q;
+    
+//     public int[] solution(int[] progresses, int[] speeds) {
+        
+//         q = new LinkedList<>();
+//         ArrayList<Integer> list = new ArrayList<>();
+//         ArrayList<Integer> list2 = new ArrayList<>();
+        
+//         for(int i=0; i < progresses.length; i++){
+//             q.offer(new Info(progresses[i], speeds[i]));
+//         }
+        
+//         while(!q.isEmpty()){
+//             Info info = q.peek();
+//             list.add(chkClear(info));
+//             q.poll();
+//         }
+//         int k = 1;
+//         int tmp = list.get(0);
+//         for(int i=1; i < list.size(); i++){
+//             if(tmp > list.get(i)){
+//                 k++;
+//             }
+//             else{
+//                 tmp = list.get(i);
+//                 list2.add(k);
+//                 k = 1;
+//             }
+//         }
+//         list2.add(k);
+        
+//         int[] answer = new int[list2.size()];
+        
+//         for(int i=0; i < list2.size(); i++){
+//             answer[i] = list2.get(i);
+//         }
+        
+//         return answer;
+//     }
+    
+//     public int chkClear(Info info){
+//         int cnt = 0;
+//         boolean flag = false;
+//         while(!flag){
+//             info.pro += info.sp;
+//             cnt++;
+//             if(info.pro >= 100) flag = true;
+//         }
+//         return cnt;
+//     }
+// }
--- 프로그래머스2단계/튜플.java
+++ 프로그래머스2단계/튜플.java
@@ -1,25 +1,36 @@
 import java.util.*;
 
 //셀수있는 순서있는 모음 = 튜플
-// 중복 가능 순열
+// 중복 가능 순열, 순서 다르면 다른 튜플
 // s가 표현하는 집합을 출력
-// 11/9 내일와서 튜플부터 다시하기
+// a1 a1a2 a1a2a3  a1a2a3a4
+// 근데 집합기호 있으면 순서 섞힐 수 있음..?
+// 11/11  저 머시기냐 저거 정규식표현? 저거만 하면 됨
 
 class Solution {
-    public int[] solution(String s) {
-        int[] answer = {};
-        
-        String[] str = s.split("},");
+    public ArrayList<Integer> solution(String s) {
+
         Map<String, Integer> map = new HashMap<>();
-        for(int j=0; j < str.length; j++){
-            System.out.println(str[j]);
+        
+        s = s.replaceAll("[\\{,\\}]", " ");
+        String[] sa = s.split("[ ]+");
             
-            for(int k = 0; k < str[j].length(); k++){
-                char ch = str[j].charAt(k);
-                if(c
+        for(int j=1; j < sa.length; j++){
+            if(!map.containsKey(sa[j]) && sa[j] != ""){
+                map.put(sa[j],1);
             }
+            else map.put(sa[j], map.get(sa[j])+1);
         }
         
+        List<String> keySetList = new ArrayList<>(map.keySet());
+            Collections.sort(keySetList, (o1,o2) -> (map.get(o2).compareTo(map.get(o1))));
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(String str : keySetList){
+            int tmp = Integer.parseInt(str);
+            answer.add(tmp);   
+        }
         
         return answer;
     }
