e2df13a98146f026c484050f6b68610966831d46
Piousangel <yyh7654@gmail.com>
2021-11-15 00:07:49 +0900

    11/15

--- 프로그래머스3단계.../기지국설치.java
+++ 프로그래머스3단계.../기지국설치.java
@@ -1,48 +1,42 @@
 import java.util.*;
 
+// 문제 보면 w만큼 boolean 이나 1로 바꿔서 chk해주고
+// w만큼 영향력 행사해야하는데... 세트로 묶어야하나..
+// 다 방문하면 그 횟수가 가장 작은 값 리턴하면 댐
 
-// 5g 수요가 높아져 4 -> 5로바꾸려고함
-// 5g는 어떤 아파트에는 전파가 도달하지 않음 전파 도달 거리 = W
-// 최소개의 기지국을 리턴
-// dfs로 풀면 될 것 같은데 w에 신경을써서 다시 풀어보겠음... 내일 다시... 11/12를 기약
-
-class 기지국설치 {
+class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-    
-        int[] arr = new int[n]; //0~10;
         
-        for(int i=0; i < arr.length; i++){
-            int cnt = 0;
-            for(int j=0; j < stations.length; j++){
-                int tmp = stations[j]-1;
-                arr[tmp] = 1;
-                if(tmp > w)
-                arr[tmp-1] = 1;
-                if(tmp < n -2){
-                    arr[tmp+1] = 1;
-                }
-                
-                dfs(arr, i, cnt);
-            }
-            
+        int[] arr = new int [n]; // 예시에선 1->0, 11->10
+        
+        for(int i : stations){
+            arr[i-1] = 1;
+            dfs(arr, i, w, 0);
         }
         
-        for(int i=0; i < arr.length; i++){
-            System.out.print(arr[i]+ " ");
+        for(int i : arr){
+            System.out.print(i+" ");
         }
-
+        
         return answer;
     }
     
-    public void dfs(int[] arr, int start, int cnt){
+    public void dfs(int[] arr, int idx, int w, int cnt){
         
-        
-        for(int i=0; i < arr.length; i++){
-            if(arr[i] != 0){
-                arr[i] = 1;
-                
-            }
+        if(cnt == w){
+            return;
         }
-    }
+        
+        if(idx-1 >= 0){
+            arr[idx-1-1] = 1;
+            dfs(arr, idx-1, w, cnt+1);
+        }
+        
+        if(idx+1 <= arr.length){
+            arr[idx+1-1] = 1;
+            dfs(arr, idx+1, w, cnt+1);
+        }
+        
+    } 
 }
