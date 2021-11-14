99e8a3e2b3ad8d9f594a31ae518ad18c137c1989
Piousangel <yyh7654@gmail.com>
2021-11-14 22:55:44 +0900

    11/14

--- 프로그래머스3단계.../2X타일링.java
+++ 프로그래머스3단계.../2X타일링.java
@@ -2,36 +2,24 @@ import java.util.*;
 
 //가로 2 세로 1 직사각형 타일
 //직사각형의 가로의 길이 n이 주어질 때 직사각형을 채우는 방법의 수
-// 1,2를 사용해서 가로길이만큼 채우는 순열 구하면 될 것같은데..?
 // 차피 세로는 엄청 늘려서라도 완성 시킬듯
-
-//거스름돈 문제랑 같은 것 같은데 DP문제인듯...;;
-// 4 -> 1111, 112, 121, 211, 22 
-// 근데 진짜 점화식 못세우는듯...
-//1  1 1 1 1
-//2  2 2
-//1,2
-
+//    1 2 3 4 5 6 7 8
+// 1  1 1 1 1 1 1 1 1
+// 2  0 1 0 1 0 1 0 1
+//1,2 1 1 2 3 5 8 13 21 ... 피보나치
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         
+        int[] arr = new int[100001];
         
-        int[] tile = new int[] {1,2};
-        int[] d = new int[n+1];
+        arr[0] = 1;
+        arr[1] = 1;
         
-        for(int i = 0; i<= n ;i++){
-             if(i % tile[0] == 0)
-                 d[i] = 1;
+        for(int i=2; i <= n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+            arr[i] = arr[i]%1000000007;
         }
         
-        for(int i=1; i < tile.length ; i++){
-            for(int j=tile[i]; j<=n; j++){
-                d[j] += d[j-tile[i]];
-            }
-        }
-        
-        answer = (int)(d[n] % 1000000007);
-        return answer;
+        return arr[n];
     }
 }
