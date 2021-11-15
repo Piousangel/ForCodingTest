21d23d9d191172f670bbe38c005ae5bea9ff7e91
Piousangel <yyh7654@gmail.com>
2021-11-15 13:59:38 +0900

    11/15

--- 프로그래머스2단계/문자열압축.java
+++ 프로그래머스2단계/문자열압축.java
@@ -2,74 +2,50 @@ import java.util.*;
 
 //늘려나갈 글자수 -> 다음 글자수가 같은지 비교
 //결과값       10/10  10/22 q;lwhflqwel ㅡ.ㅡ 안풀러ㅕㅓㅕㅕㅕ
-
+// 11/15 다시..
 class Solution {
-    int len = 0;
     public int solution(String s) {
-        int answer = 0;
-        int cnt = 1000;
-        int tmp = 0;
-
-        int maxCut = 0;
+        int answer = 100001;
         
-        if(s.length() % 2 == 0) maxCut = s.length()/2;
-        else maxCut = (s.length()-1) /2;
-        
-        for(int i=1; i <= maxCut; i++){
-            len = lenCnt(i,s);  //몇개씩 커팅할지
-            tmp = Math.min(cnt, len);
+        for(int i=1; i <= s.length()/2; i++){  //몇개 단위로 끊을 것인지
+            int len = chkStr(s, i);
+            answer = Math.min(answer, len);
         }
-        answer = tmp;
         return answer;
     }
     
-    public int lenCnt(int cut, String s){
-        int cnt = s.length();
-        //String last = s.subString(cut, s.length);
-        
-        for(int i=0; i < maxCut; i++){           //자를 때 범위 수정해야함... 예를들어 앞에서 10개 잘랐는데 뒤에 10개가 없으면 못잘라요...
-            String str = s.substring(i,i+cut);
-            if(str.equals(s.substring(i+cut,i+cut+cut)) && str.length() <= cnt/2 ){
-                cnt = cnt - 1;
+    public int chkStr(String s, int idx){
+        String ans = "";
+        Stack<String> st = new Stack<>();
+        ArrayList<String> list = new ArrayList<>();
+        int cnt = 0;
+        String str = "";
+        for(int i=0; i < s.length()-idx+1; i+=idx){
+            str = s.substring(i,i+idx);
+            if(st.size() == 0){
+                st.push(str);
+                cnt++;
             }
+            else if(st.size() != 0 && st.peek().equals(str)){
+                cnt++;
+            }
+            else if(!st.peek().equals(str)){
+                if(cnt != 1)
+                    list.add(Integer.toString(cnt));
+                list.add(st.peek());
+                st.push(str);
+                cnt = 1;          
+            }   
         }
-        return cnt;
+        //if(cnt != 1)
+            list.add(Integer.toString(cnt));
+        list.add(str);
+        
+        for(int i=0; i < list.size(); i++){
+            ans += list.get(i);
+        }
+        
+        //System.out.println(ans);
+        return ans.length();
     }
-    
 }
-
-
-
-// class Solution {
-//     public int solution(String s) {
-//         int answer = 0;
-        
-//         int maxCut = 0;
-        
-//         if(s.length() % 2 == 0) maxCut = s.length()/2;
-//         else maxCut = (s.length()-1) /2;
-        
-//         for(int i=1; i < maxCut; i++){
-            
-//             String str1 = s.substring(0,i);
-//             String str2 = s.substring(i,s.length());
-//             reTry(str1, str2);
-//             System.out.print(str1+ " ");
-//             System.out.println(str2);
-//         }
-        
-//         return answer;
-//     }
-    
-//     public int reTry(String str1, String str2){
-//         int cnt = 0;
-//         String str3 = str2.substring(0,str1.length());  // 똑같이 자름
-//         String str4 = str2.substring(str1.length(), str2.length());
-//         if(str1.equals(str3)){
-//             reTry(str3,str4);
-//         }
-//         else{
-            
-//         }
-//     }
-// }
--- 프로그래머스3단계.../기지국설치.java
+++ 프로그래머스3단계.../기지국설치.java
@@ -3,6 +3,7 @@ import java.util.*;
 // 문제 보면 w만큼 boolean 이나 1로 바꿔서 chk해주고
 // w만큼 영향력 행사해야하는데... 세트로 묶어야하나..
 // 다 방문하면 그 횟수가 가장 작은 값 리턴하면 댐
+// 다...시... 11/15
 
 class Solution {
     public int solution(int n, int[] stations, int w) {
@@ -12,11 +13,24 @@ class Solution {
         
         for(int i : stations){
             arr[i-1] = 1;
-            dfs(arr, i, w, 0);
+            plusArea(arr, i, w, 0);
         }
         
         for(int i : arr){
-            System.out.print(i+" ");
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+        
+        for(int i=0; i < arr.length; i++){
+            if(arr[i] == 0){
+                answer++;
+                dfs(arr, i, w, 0);
+            }
+        }
+        
+        for(int i : arr){
+            System.out.print(arr[i] + " ");
         }
         
         return answer;
@@ -24,19 +38,35 @@ class Solution {
     
     public void dfs(int[] arr, int idx, int w, int cnt){
         
+        if(cnt == w) return;
+        
+        for(int i=0; i < w; i++){
+            if(arr[i] != 1){
+                arr[i] = 1;
+                
+                if(idx-1 >= 0){
+                    arr[idx-1] = 1;
+                    dfs(arr, idx-1, w, cnt+1);
+                }  
+                if(idx+1 <= arr.length){
+                    arr[idx+1] = 1;
+                    dfs(arr, idx+1, w, cnt+1);
+                }
+            }
+        }   
+    }
+    
+    public void plusArea(int[] arr, int idx, int w, int cnt){  
         if(cnt == w){
             return;
-        }
-        
+        }  
         if(idx-1 >= 0){
             arr[idx-1-1] = 1;
-            dfs(arr, idx-1, w, cnt+1);
-        }
-        
+            plusArea(arr, idx-1, w, cnt+1);
+        }  
         if(idx+1 <= arr.length){
             arr[idx+1-1] = 1;
-            dfs(arr, idx+1, w, cnt+1);
+            plusArea(arr, idx+1, w, cnt+1);
         }
-        
-    } 
+    }
 }
