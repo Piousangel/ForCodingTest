344cecd9a6ab27c1304fd12f533719c839157816
Piousangel <yyh7654@gmail.com>
2021-11-17 16:11:07 +0900

    11/17

--- 프로그래머스2단계/카펫.java
+++ 프로그래머스2단계/카펫.java
@@ -1,27 +1,50 @@
 //노란색 갈색 격자수는 기억함
-// 9/10  11/16
-class 카펫 {
+// 9/10  11/16 11/17
+
+class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int row = 0;
-        int col = 0;
-        //전체 카펫수 = brown+yellow
-        //가로 col , 세로 row n이라고 하면 , 가로길이는 세로길이와 같거나 길다.
-        // (row+col)*2 - 4 = brown
-        // row*col = brwon + yellow
-        // (row - 2)*(col-2) = yellow // row*col -2(row+col) + 4 = yellow
         int total = brown + yellow;
-        for(int i= 1; i <= total; i++){
-            col = i;    //가로길이는 세로길이와 같거나 크므로 1부터
-            row = total/i;
+        for(int i=1; i <= total; i++){
+            int col = i;  //가로의 길이가 세로길이와 같거나 큼
+            int row = total/i;
             
             if(row > col) continue;
             
-            if((row-2)*(col-2) == yellow){
+            //brown = row*col - 4 - yellow
+            //yellow = (col - 2)(row-2)
+            if((col-2)*(row-2) == yellow){
                 answer[0] = col;
                 answer[1] = row;
             }
+            
         }
+        
         return answer;
     }
 }
+// class 카펫 {
+//     public int[] solution(int brown, int yellow) {
+//         int[] answer = new int[2];
+//         int row = 0;
+//         int col = 0;
+//         //전체 카펫수 = brown+yellow
+//         //가로 col , 세로 row n이라고 하면 , 가로길이는 세로길이와 같거나 길다.
+//         // (row+col)*2 - 4 = brown
+//         // row*col = brwon + yellow
+//         // (row - 2)*(col-2) = yellow // row*col -2(row+col) + 4 = yellow
+//         int total = brown + yellow;
+//         for(int i= 1; i <= total; i++){
+//             col = i;    //가로길이는 세로길이와 같거나 크므로 1부터
+//             row = total/i;
+            
+//             if(row > col) continue;
+            
+//             if((row-2)*(col-2) == yellow){
+//                 answer[0] = col;
+//                 answer[1] = row;
+//             }
+//         }
+//         return answer;
+//     }
+// }
--- 프로그래머스2단계/타겟넘버DFS.java
+++ 프로그래머스2단계/타겟넘버DFS.java
@@ -1,17 +1,19 @@
 import java.util.*;
 
-//11/10 11/11
+//11/10 11/11 11/17
 
-class Solution {
-    int answer = 0;
+class 타겟넘버DFS {
     public int solution(int[] numbers, int target) {
+        answer = 0;
         dfs(numbers, target, 0, 0);
+        
         return answer;
     }
-    
+    public int answer;
     public void dfs(int[] numbers, int target, int idx, int sum){
-        
+       
         if(idx == numbers.length){
+            //System.out.println("sum" + sum);
             if(sum == target){
                 answer++;
             }
@@ -21,7 +23,7 @@ class Solution {
         sum += numbers[idx];
         dfs(numbers, target, idx+1, sum);
         sum -= numbers[idx];
-        sum += (-1*numbers[idx]);
+        sum += (-1 * numbers[idx]);
         dfs(numbers, target, idx+1, sum);
     }
 }
