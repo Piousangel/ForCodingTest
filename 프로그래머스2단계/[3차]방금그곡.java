ed5aac86d09675b3ef1fbc6ae981922ca5843b01
Piousangel <yyh7654@gmail.com>
2021-11-19 00:32:43 +0900

    11/19

--- 프로그래머스2단계/[3차]방금그곡.java
+++ 프로그래머스2단계/[3차]방금그곡.java
@@ -6,15 +6,14 @@ import java.util.*;
 // 재생 시간도 같으면 먼저 입력된 제목 반환
 // 없으면 " (None) " 반환 1031
 // 열심히햇들,,.에러가나오네요... 11/15
-// 뒤짝에 else부분을 강화시켜줘야해  11/19
+// 뒤짝에 else부분을 강화시켜줘야해  11/19 5 try -> ㅅ ㅅ ㅅ ㅅ
 
 class Solution {
     public String solution(String m, String[] musicinfos) {
         String answer = "";
-        ArrayList<String> list = new ArrayList<>();
+        int maxPlayTime = -1;
         
         for(int i=0; i < musicinfos.length; i++){
-            
             String[] str = musicinfos[i].split(",");
             
             // for(String s : str){
@@ -24,6 +23,7 @@ class Solution {
             int hour = Integer.parseInt(str[1].split(":")[0]) - Integer.parseInt(str[0].split(":")[0]);
             int min = Integer.parseInt(str[1].split(":")[1]) - Integer.parseInt(str[0].split(":")[1]);
             int time = hour*60 + min;
+            
             // System.out.print("hour : " + hour+" ");
             // System.out.println("min : " + min);
             
@@ -42,31 +42,90 @@ class Solution {
             }
             
             m = changeStr(m);   //m 변환
-            // System.out.print("m : " + m + " ");  
-            //System.out.println(melody);
             
-            // if(m.length() < melody.length()){    //만약 모두 포함되면 재생시간이 긴 음악 반환
-            //     if(melody.contains(m)){
-            //         //list.add(melody);
-            //         answer = str[2];
-            //     }
-            // }
-            
-            
-            
-            // else{
-            //     if(m.contains(melody)){
-            //         //list.add(melody);
-            //         answer = str[2];
-            //     }
-            // }
-            
+            if (melody.contains(m) && time > maxPlayTime) {
+                answer = str[2];
+                maxPlayTime = time;
+            }
+
         }
-        int len = 0;
         
-        for(String s : list){
-            System.out.println(s);
-        }
+        if(answer == "") answer = "(None)";
+        
+        return answer;
+    }
+    
+    public String changeStr(String str){
+        
+        str = str.replaceAll("C#","H");
+        str = str.replaceAll("D#","I");
+        str = str.replaceAll("F#","J");
+        str = str.replaceAll("G#","K");
+        str = str.replaceAll("A#","L");
+        
+        return str;
+    }
+}
+
+// class Solution {
+//     public String solution(String m, String[] musicinfos) {
+//         String answer = "";
+//         ArrayList<String> list = new ArrayList<>();
+        
+//         for(int i=0; i < musicinfos.length; i++){
+            
+//             String[] str = musicinfos[i].split(",");
+            
+//             // for(String s : str){
+//             //     System.out.println(s);
+//             // }
+            
+//             int hour = Integer.parseInt(str[1].split(":")[0]) - Integer.parseInt(str[0].split(":")[0]);
+//             int min = Integer.parseInt(str[1].split(":")[1]) - Integer.parseInt(str[0].split(":")[1]);
+//             int time = hour*60 + min;
+//             // System.out.print("hour : " + hour+" ");
+//             // System.out.println("min : " + min);
+            
+//             String musicStr = str[3];
+//             musicStr = changeStr(musicStr);
+//             //System.out.println(musicStr);
+//             String melody = "";
+//             int idx = 0;
+            
+//             for(int j=0; j < time; j++){         //멜로디 변환까지 완료
+//                 if(idx > musicStr.length()-1){
+//                     idx = idx%musicStr.length();
+//                 }
+//                 melody += musicStr.charAt(idx);
+//                 idx++;
+//             }
+            
+//             m = changeStr(m);   //m 변환
+//             // System.out.print("m : " + m + " ");  
+//             //System.out.println(melody);
+            
+//             // if(m.length() < melody.length()){    //만약 모두 포함되면 재생시간이 긴 음악 반환
+//             //     if(melody.contains(m)){
+//             //         //list.add(melody);
+//             //         answer = str[2];
+//             //     }
+//             // }
+            
+            
+            
+//             // else{
+//             //     if(m.contains(melody)){
+//             //         //list.add(melody);
+//             //         answer = str[2];
+//             //     }
+//             // }
+            
+//         }
+//         int len = 0;
+        
+//         for(String s : list){
+//             System.out.println(s);
+//         }
         
         // if(list.size() == 2){
         //     for(int i=0; i < list.size(); i++){
