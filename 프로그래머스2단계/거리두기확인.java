d02bb14377fc12941441a16ca9b60de252773e9d
Piousangel <yyh7654@gmail.com>
2021-11-18 17:00:49 +0900

    11/18

--- 프로그래머스2단계/거리두기확인.java
+++ 프로그래머스2단계/거리두기확인.java
@@ -4,65 +4,147 @@ import java.util.*;
 // p와 p 사이에 파티션X가 있고 O가 있으면 안댐
 // 5X5로 이루어져있음
 // 제대로 앉아있으면 1 아니면 0
-// 11/11 다시..
+// 11/11 다시.. 11/18 흠.. 거의..
 
-class Solution {
+class 거리두기확인 {
     
-    int[] dx = {1,0,1};
-    int[] dy = {1,1,0};
+    int[] dx = {1,0,-1,0};
+    int[] dy = {0,1,0,-1};
 
+    public class Node{
+        int x;
+        int y;
+        int cnt;
+        
+        public Node(int x, int y, int cnt){
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+    
+    Queue<Node> q;
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
-        
-        for(int i=0; i < places.length; i++){
-            String[] str = places[i];
-            boolean chkk = bfs(str);
-            if(chkk) answer[i] = 1;
-            else answer[i] = 0;
+        char[][] ch = new char[5][5];
+        int answerIdx = 0;
+        for(int i=0; i < places.length; i++){  // 0 1 2 3 4 5
+            
+            boolean flag = false;
+            
+            for(int j=0; j < places[i].length; j++){
+                char c[] = places[i][j].toCharArray();
+                
+                for(int k =0; k < c.length; k++){
+                    ch[j][k] = c[k];
+                }
+            }
+            boolean[][] visited;
+            for(int j=0; j < ch.length; j++){
+                for(int k=0; k < ch[j].length; k++){
+                    visited = new boolean[5][5];
+                    if(ch[j][k] == 'P'){
+                        visited[j][k] = true;
+                        if(!bfs(ch, visited, j, k)){
+                            answer[answerIdx++] = 0;
+                            continue;
+                        }
+                    }
+                }
+            }
+            answer[answerIdx++] = 1;
         }
-        
         return answer;
     }
     
-    public boolean bfs(String[] str){
-        char[][] chBox = new char[5][5];
-        boolean f = true;
-        for(int i=0; i < str.length; i++){
-            char[] ch = str[i].toCharArray();
-            
-            for(int j=0; j < ch.length; j++){
-                chBox[i][j] = ch[j];  
-            }
-        }
+    public boolean bfs(char[][] ch, boolean[][] visited, int m, int n){
+        q = new LinkedList<>();
         
-        for(int i=0; i < 5; i++){
-            for(int j=0; j < 5; j++){
-                if(chBox[i][j] == 'P'){
-                    boolean[][] visited = new boolean[5][5];
-                    boolean flag  = chkPos(chBox, visited, i, j);
-                    if(!flag) f = false;
+        q.offer(new Node(m,n,0));
+        
+        while(!q.isEmpty()){
+            
+            Node node = q.poll();
+            
+            if(node.cnt != 0 && ch[node.x][node.y] == 'P'){
+                if(node.cnt <= 2) return false;
+            }
+            if(node.cnt > 2) return true;
+            
+            for(int i=0; i < 4; i++){
+                
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+                
+                if(0 <= nx && nx < 5 && 0 <= ny && ny < 5){
+                    if(visited[nx][ny] != true && ch[nx][ny] != 'X'){
+                        visited[nx][ny] = true;
+                        q.offer(new Node(nx,ny,node.cnt+1));
+                    }
                 }
             }
         }
-        return f;
-    }
-    
-    public boolean chkPos(char[][] arr, boolean[][] visited, int m, int n){
-        visited[m][n] = true;
-        boolean chk = false;
-        
-        for(int i=0; i < 3; i++){
-            int nx = m + dx[i];
-            int ny = n + dy[i];
-            
-            if(nx >= 0 && nx < 5 && ny >= 0 && ny < 5){
-                if(visited[nx][ny] != true && arr[nx][ny] != 'P'){
-                    visited[nx][ny] = true;
-                    chk = true;
-                }
-                else chk = false;
-            }
-        }
-        return chk;
+        return false;      
     }
 }
+
+// class Solution {
+    
+//     int[] dx = {1,0,1};
+//     int[] dy = {1,1,0};
+
+//     public int[] solution(String[][] places) {
+//         int[] answer = new int[places.length];
+        
+//         for(int i=0; i < places.length; i++){
+//             String[] str = places[i];
+//             boolean chkk = bfs(str);
+//             if(chkk) answer[i] = 1;
+//             else answer[i] = 0;
+//         }
+        
+//         return answer;
+//     }
+    
+//     public boolean bfs(String[] str){
+//         char[][] chBox = new char[5][5];
+//         boolean f = true;
+//         for(int i=0; i < str.length; i++){
+//             char[] ch = str[i].toCharArray();
+            
+//             for(int j=0; j < ch.length; j++){
+//                 chBox[i][j] = ch[j];  
+//             }
+//         }
+        
+//         for(int i=0; i < 5; i++){
+//             for(int j=0; j < 5; j++){
+//                 if(chBox[i][j] == 'P'){
+//                     boolean[][] visited = new boolean[5][5];
+//                     boolean flag  = chkPos(chBox, visited, i, j);
+//                     if(!flag) f = false;
+//                 }
+//             }
+//         }
+//         return f;
+//     }
+    
+//     public boolean chkPos(char[][] arr, boolean[][] visited, int m, int n){
+//         visited[m][n] = true;
+//         boolean chk = false;
+        
+//         for(int i=0; i < 3; i++){
+//             int nx = m + dx[i];
+//             int ny = n + dy[i];
+            
+//             if(nx >= 0 && nx < 5 && ny >= 0 && ny < 5){
+//                 if(visited[nx][ny] != true && arr[nx][ny] != 'P'){
+//                     visited[nx][ny] = true;
+//                     chk = true;
+//                 }
+//                 else chk = false;
+//             }
+//         }
+//         return chk;
+//     }
+// }
--- 프로그래머스3단계.../단속카메라.java
+++ 프로그래머스3단계.../단속카메라.java
@@ -3,38 +3,45 @@ import java.util.*;
 //한 기준점을 잡고 그것을 기준으로 왼쪽, 오른쪽 최솟값을 구해
 // 그리고 그 값이 기준값 보다 모두 작지 않으면 남을 수 있는 풍선임
 // 보니까 맨왼쪽 맨 오른쪽은 무조건 가능
-// 11/5 11/18
+// 11/5 11/18 11/18 ㅅ ㅅ ㅅ ㅅ ㅅ
 
-class 단속카메라 {
+class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
+        int answer = 1;
         
-        for(int i=0; i < routes.length; i++){
-            int start = routes[i][0];
-            int end = routes[i][1];
+        ArrayList<int[]> list = new ArrayList<>();
+        
+        for(int[] tmp : routes){
+            list.add(tmp);
+        }
+        
+        Collections.sort(list, (a,b) -> a[1] - b[1]); // 작은 것 부터 정렬
+        
+        // for(int i=0; i < list.size(); i++){
+        //     System.out.print(list.get(i)[0]+" ");
+        //     System.out.println(list.get(i)[1]);
+        // }
+        
+        int camSet = list.get(0)[1];
+        
+        while(list.size() > 1){
             
-            for(int j=start; j <= end; j++){
-                if(!map.containsKey(j)){
-                    map.put(j,1);
+            for(int i=0; i < list.size(); i++){
+                int[] arr = list.get(i);
+                
+                if(arr[0] <= camSet){
+                    list.remove(arr);
+                    i--;
+                    continue;
                 }
-                else map.put(j, map.get(j)+1);
+                else{
+                    answer++;
+                    camSet = arr[1];
+                }
+                
             }
         }
-        
-        List<Integer> keySetList = new ArrayList<>(map.keySet());
-            Collections.sort(keySetList, (o1,o2) -> (map.get(o2).compareTo(map.get(o1))));
-        
-        int cnt = 0;
-        for(int tmp : keySetList){
-            
-            if(answer < routes.length){
-                answer += map.get(tmp);
-                cnt++;
-            }
-        }
-        
-        return cnt;
+        return answer;
     }
 }
 // 다시다시
