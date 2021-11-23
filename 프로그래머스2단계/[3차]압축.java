156e449ff5b1865877ab30515be2079bb52c8645
Piousangel <yyh7654@gmail.com>
2021-11-23 22:54:44 +0900

    11/23

--- 프로그래머스2단계/[3차]압축.java
+++ 프로그래머스2단계/[3차]압축.java
@@ -1,58 +1,60 @@
 import java.util.*;
 
-//11/23 저녁먹고다시
+//11/23 저녁먹고다시 11/23
 
 class Solution {
     
     String[] alpha = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
     ,"P","Q","R","S","T","U","V","W","X","Y","Z"};
     
-    ArrayList<Integer> list;
+    ArrayList<Integer> list = new ArrayList<>();
     Map <String, Integer> map;
-    int index;
+ 
     public int[] solution(String msg) {
-        list = new ArrayList<>();
         map = new HashMap<>();
-        index = 1;
         
         for(int i=0; i < alpha.length; i++){
-            map.put((alpha[i]),index++);
+            map.put((alpha[i]), i+1);
         }
+        
+        int strLen = msg.length();
+
+        for(int i=0; i < strLen; i++){
+            String key = Character.toString(msg.charAt(i));
+            int index = i+1;
             
-        for(int i=0; i < msg.length()-2; i++){        
-            dfs(msg, i, i+2);
+            while(index <= strLen) {
+                
+				if(index == strLen) {
+                    //System.out.print("msg.substring(i)" + msg.substring(i, strLen-1)); 
+					list.add(map.get(msg.substring(i, strLen)));
+					i = index;
+					break;
+				}
+
+				String nextKey = msg.substring(i, index+1); //++시켜 여기서 확인
+
+				if(map.containsKey(nextKey)) { 
+					index++;
+				} else { 
+					key = msg.substring(i, index);
+					list.add(map.get(key)); 
+					map.put(nextKey, map.size()+1); 
+					i = index-1;
+					break;
+				}
+			}
         }
         
-        int[] answer = new int[list.size()];
-        
+        int[] answer = new int[list.size()];   
         for(int i=0; i < list.size(); i++){
             answer[i] = list.get(i);
         }
         
         return answer;
-    }
-    
-    public void dfs(String msg, int start, int end){
-    
-        String str = msg.substring(start, end);
-        
-        if(map.containsKey(str)){
-            list.add(map.get(str));
-            if(end+1 < msg.length()){
-                dfs(msg,start,end+1);
-            }
-        }
-        else{
-            list.add(map.get(msg.substring(start, end-1)));
-            map.put(str,index++);
-            return;
-              
-        }
-    }
-    
 
+    }
 }
-
 
 // class Solution {
     
